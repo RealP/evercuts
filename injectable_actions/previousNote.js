75ffe45e5f29a7eb488be0093443b62d4843539c
Paul Pfeffer <mr.paul16@gmail.com>
2020-09-04 16:41:03 -0700

    Fix next note and previous note (#11)
    These are going to continue breaking when evernote UI changes.
    I can try and keep up for now

--- injectable_actions/previousNote.js
+++ injectable_actions/previousNote.js
@@ -1,6 +1,7 @@
-// Get focused element hack (works as of 6/1/2020)
+// Get focused element hack (works as of 9/4/2020)
 // The element in focus has the following css selector qC87s8opH1X3hjuzPoKrl
 var ele = document.querySelector(".qC87s8opH1X3hjuzPoKrl");
-var prev_ele = ele.previousSibling;
+// Hack to find the next note. This will break soon. Already broke once...
+var prev_ele = ele.parentElement.previousSibling.firstChild;
 var id = prev_ele.id.split("_")[0];
 window.location.href = `https://www.evernote.com/client/web?login=true#?fs=false&n=${ id }`;

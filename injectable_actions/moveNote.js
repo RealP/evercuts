32e05e269a056b756ac9725ebcc6c0014e12403b
RealP <mr.paul16@gmail.com>
2020-11-18 19:32:44 -0800

    Fixes issue with not opening menu

--- injectable_actions/moveNote.js
+++ injectable_actions/moveNote.js
@@ -1,12 +1,14 @@
-function waitAndClickMove (attempts) {
+function clickMove () {
     const el = document.getElementById('qa-ACTION_MOVE');
     el.click();
 }
 
-function openMoveNoteDialog() {
-    // Open actions
+function openMoreActionsMenu() {
+    var evt = document.createEvent("MouseEvents");
+    evt.initEvent("mouseup", true, true);
     const button = document.getElementById("qa-NOTE_ACTIONS").parentElement;
-    button.click();
-    setTimeout(function() { waitAndClickMove(0);}, 100); // Wait .1 sec
+    button.dispatchEvent(evt);
 }
-openMoveNoteDialog();
+
+openMoreActionsMenu();
+setTimeout(function() { clickMove();}, 100); // Wait .1 sec

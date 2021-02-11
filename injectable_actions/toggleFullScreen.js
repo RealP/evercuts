5b4401eef313efb72a009ed15eaf3dfc44a33333
RealP <mr.paul16@gmail.com>
2021-02-10 18:23:20 -0800

    Full screen is toggle with mousedown now for whatever reason

--- injectable_actions/toggleFullScreen.js
+++ injectable_actions/toggleFullScreen.js
@@ -1,5 +1,7 @@
 function clickFullScreen() {
-    var button = document.getElementById("qa-NOTE_FULLSCREEN_BTN");
-    button.click();
+    let evt = document.createEvent("MouseEvents");
+    evt.initEvent("mousedown", true, true);
+    const button = document.getElementById("qa-NOTE_FULLSCREEN_BTN");
+    button.dispatchEvent(evt);
 }
 clickFullScreen();

a599165b3f3824de2cb331381b78993ea08f3184
Paul Pfeffer <mr.paul16@gmail.com>
2021-07-18 18:01:00 -0700

    Add shortcut to show popup (#23)
    * Add option to open popup with keyboard via manifest
    * Description for showing popup
    * version bump to 1.11

--- popup/index.js
+++ popup/index.js
@@ -1,16 +1,21 @@
 window.onload = (event) => {
-    var table = document.getElementById("shortcutTable");
-    chrome.commands.getAll(function(commands){
-      for (var i = 0; i <= commands.length; i++) {
-        var row = table.insertRow(i+1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        cell1.innerHTML = commands[i]["description"];
-        if (commands[i]["shortcut"]) {
-            cell2.innerHTML = commands[i]["shortcut"];
-        } else {
-            cell2.innerHTML = "Click button below to configure";
-        }
+  let table = document.getElementById("shortcutTable");
+  let rowIdx = 0;
+  chrome.commands.getAll(function (commands) {
+    for (let i = 0; i <= commands.length; i++) {
+      if (commands[i]["description"] === "") {
+        commands[i]["description"] = "Show this popup";
       }
-    });
+      let row = table.insertRow(rowIdx + 1);
+      let cell1 = row.insertCell(0);
+      let cell2 = row.insertCell(1);
+      cell1.innerHTML = commands[i]["description"];
+      if (commands[i]["shortcut"]) {
+        cell2.innerHTML = commands[i]["shortcut"];
+      } else {
+        cell2.innerHTML = "Click button below to configure";
+      }
+      rowIdx += 1;
+    }
+  });
 };

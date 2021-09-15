cb4607a8154c6d3a31e6e4a0806de3e7ef13bf8c
Paul Pfeffer <mr.paul16@gmail.com>
2021-09-14 21:51:59 -0700

    Removes two actions which no longer worked (#24)
    * next note
    * previous note

--- injectable_actions/nextNote.js
+++
@@ -1,8 +0,0 @@
-// Get focused element hack (works as of 9/4/2020)
-// The element in focus has the following css selector qC87s8opH1X3hjuzPoKrl
-var ele = document.querySelector(".qC87s8opH1X3hjuzPoKrl");
-// Hack to find the next note. This will break soon. Already broke once...
-var next_ele = ele.parentElement.nextSibling.firstChild;
-var id = next_ele.id.split("_")[0];
-// We got the new note id lets navigate to it
-window.location.href = `https://www.evernote.com/client/web?login=true#?fs=false&n=${ id }`;
--- injectable_actions/previousNote.js
+++
@@ -1,7 +0,0 @@
-// Get focused element hack (works as of 9/4/2020)
-// The element in focus has the following css selector qC87s8opH1X3hjuzPoKrl
-var ele = document.querySelector(".qC87s8opH1X3hjuzPoKrl");
-// Hack to find the next note. This will break soon. Already broke once...
-var prev_ele = ele.parentElement.previousSibling.firstChild;
-var id = prev_ele.id.split("_")[0];
-window.location.href = `https://www.evernote.com/client/web?login=true#?fs=false&n=${ id }`;
